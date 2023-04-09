e33f92b64672ca1237db018bf0a516ee4e898a72
Cadenmf24 <cadenmf24@gmail.com>
2023-04-09 14:32:40 -0400

    barebones strategy implemented

---
+++ Nutriapp2/State/LoseWeight.java
@@ -0,0 +1,29 @@
+package Nutriapp2.State;
+
+public class LoseWeight implements GoalState{
+
+    @Override
+    public String toString() {
+        return "Goal Set to [LoseWeight]";
+    }
+
+    @Override
+    public void handleWeightGain() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleWeightGain'");
+    }
+
+    @Override
+    public void handleWeightLoss() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleWeightLoss'");
+    }
+
+    @Override
+    public void setGoal(GoalState goalState) {
+        goalState.setGoal(this);
+        throw new UnsupportedOperationException("Unimplemented method 'setGoal'");
+    }
+}
+
+
---
+++ Nutriapp2/Strategy/MaintainWeight.java
@@ -0,0 +1,19 @@
+package Nutriapp2.Strategy;
+
+public class MaintainWeight implements StrategyGoal{
+
+    private int desiredCalories;
+    private int desiredWeight;
+
+    @Override
+    public void setStrategy(StrategyGoal strategyGoal) {
+        strategyGoal.setStrategy(this);
+    }
+
+    @Override
+    public int execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
