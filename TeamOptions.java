8486967b617271f0f520bb9d7490f16ce2019082
Yi Xhan <yl9431@rit.edu>
2023-04-17 19:25:04 -0400

    added some implementation, but since this is a mediator, requires some implementation from colleagues(users)

--- TeamOptions.java
+++ TeamOptions.java
@@ -1,10 +1,14 @@
-import java.util.ArrayList;
-
 public interface TeamOptions {
     
-    public void joinTeam();
-    public void sendInvite();
-    
+    public void joinTeam(User user);
+    public void leaveTeam(User user);
+    public void sendInvite(User sender, User receiver);
+    public void viewHistory(User user);
+    public void logWorkout();
 
+    public void sendNotification();
+
+    public void setChallenge(Integer minutes);
+    public void viewRanking();
 
 }
--- Teams.java
+++ Teams.java
@@ -1,45 +1,76 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Teams implements TeamOptions{
 
+    private ArrayList<Users> team_members;
+    private ArrayList<Users> invited_users;
+    private boolean active_challenge = false;
+    private String current_challenge;
+    private HashMap<User, Integer > challenge_progress;
+    
+
     @Override
-    public void joinTeam() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'joinTeam'");
+    public void joinTeam(User user) {
+        // check if user is in a team, then join if not already in one
+        // maybe need to check if user is in a team on the user's side rather than here
     }
 
     @Override
-    public void sendInvite() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendInvite'");
+    public void leaveTeam(User user){
+        if(team_members.contains(user)){
+            team_members.remove(user);
+            //depends on user implementation to remove their own current team
+        }
     }
 
     @Override
-    public void viewHistory() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'viewHistory'");
+    public void sendInvite(User sender, User receiver) {
+        if(team_members.contains(sender)){
+            //sends notification to receiver that they have received a team invite
+        }
+        else{
+            System.out.println("You are not in a team! Join a team to invite others.");
+        }
+    }
+
+    @Override
+    public void viewHistory(User user) {
+        if(team_members.contains(user)){
+            //returns history from target user
+            // may request a string since a user shouldn't be able to access another user's fields
+        }
     }
 
     @Override
     public void logWorkout() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'logWorkout'");
+        // based on implementation of workout, Teams may have an arraylist of team members' workouts, and this would add to that list
+        // also would update challenge progress if there is an active challenge
     }
 
     @Override
     public void sendNotification() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendNotification'");
+        for (User curr_user : team_members) {
+            //sends notification to each team member
+            // will be called by logWorkout
+        }
     }
 
+
     @Override
-    public void setChallenge() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setChallenge'");
+    public void setChallenge(Integer minutes) {
+        if(this.active_challenge){
+            System.out.println("There is already an active challenge!");
+        }
+        else{
+            this.active_challenge = true;
+            this.current_challenge = "Active challenge of " + minutes.toString() + "minutes this week.";
+        }
     }
 
     @Override
     public void viewRanking() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'viewRanking'");
+        //would sort users by minutes, then print out rankings
     }
     
 }
