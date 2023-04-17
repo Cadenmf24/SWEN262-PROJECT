4d2625322d3243e9b34be06f71f94e4aa2bd67f1
Yi Xhan <yl9431@rit.edu>
2023-04-17 15:37:55 -0400

    initial push of unimplemented teams functions

---
+++ TeamOptions.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public interface TeamOptions {
+    
+    public void joinTeam();
+    public void sendInvite();
+    
+
+
+}
---
+++ Teams.java
@@ -0,0 +1,45 @@
+public class Teams implements TeamOptions{
+
+    @Override
+    public void joinTeam() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'joinTeam'");
+    }
+
+    @Override
+    public void sendInvite() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendInvite'");
+    }
+
+    @Override
+    public void viewHistory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'viewHistory'");
+    }
+
+    @Override
+    public void logWorkout() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'logWorkout'");
+    }
+
+    @Override
+    public void sendNotification() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendNotification'");
+    }
+
+    @Override
+    public void setChallenge() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setChallenge'");
+    }
+
+    @Override
+    public void viewRanking() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'viewRanking'");
+    }
+    
+}
